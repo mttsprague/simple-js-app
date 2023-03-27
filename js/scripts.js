46e31796feadfd7b06814d22e1354c05772f8459
mttsprague <mttsprague@gmail.com>
2023-03-27 16:44:40 -0500

    created initial pokemon list
    created pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello World');
-
-let favoriteFood = 'ice cream';
-document.write(favoriteFood);
+/*creating an intitial pokemon list*/
+    
+    let pokemonList = [
+        {name:'Pikachu', height:'0.4', type:[Electric]},
+        {name:'Abra', height:'0.9', type:[Psychic]},
+        {name:'Charizard', height:'1.7', type:['Fire', 'Flying']}
+    ];   

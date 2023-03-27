16ea69056fcca1332993a120d3bd50ff57254d61
mttsprague <mttsprague@gmail.com>
2023-03-27 18:13:36 -0500

    Added some initial styling
    initial styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 /*creating an intitial pokemon list*/
     
     let pokemonList = [
-        {name:'Pikachu', height:'0.4', type:[Electric]},
-        {name:'Abra', height:'0.9', type:[Psychic]},
-        {name:'Charizard', height:'1.7', type:['Fire', 'Flying']}
+        {name:'Pikachu', height: 0.4, type:['Electric']},
+        {name:'Abra', height: 0.9, type:['Psychic']},
+        {name:'Charizard', height: 1.7, type:['Fire', 'Flying']}
     ];   
 
 /*if...else statement that shows any pokemon greater than or equal to 1.2m*/
 
-    let pokemonBig = 'wow, this pokemon is pretty big!';
-
     for (let i=0; i < pokemonList.length; i++) {
+        let pokemonStats = '<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + 'm)';
+
         if (pokemonList[i].height >= 1.2) {
-            document.write(pokemonList[i].name + ' (height: ' + (pokemonList[i].height) + ')' + (pokemonBig) + ' <br>');
-        } else {
-            document.write(pokemonList[i].name + ' (height: ' + (pokemonList[i].height) + ')<br>');
+            pokemonStats += ' Wow, this is a pretty big Pokemon!'
         }
+
+        pokemonStats += '</p>';
+        document.write(pokemonStats);
     }

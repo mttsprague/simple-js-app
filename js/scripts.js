678e98fd318e0294cade1264adca68fc10031f0b
mttsprague <mttsprague@gmail.com>
2023-03-27 17:45:58 -0500

    if...else
    added pokemonBig and created an if else

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,15 @@
         {name:'Abra', height:'0.9', type:[Psychic]},
         {name:'Charizard', height:'1.7', type:['Fire', 'Flying']}
     ];   
+
+/*if...else statement that shows any pokemon greater than or equal to 1.2m*/
+
+    let pokemonBig = 'wow, this pokemon is pretty big!';
+
+    for (let i=0; i < pokemonList.length; i++) {
+        if (pokemonList[i].height >= 1.2) {
+            document.write(pokemonList[i].name + ' (height: ' + (pokemonList[i].height) + ')' + (pokemonBig) + ' <br>');
+        } else {
+            document.write(pokemonList[i].name + ' (height: ' + (pokemonList[i].height) + ')<br>');
+        }
+    }
